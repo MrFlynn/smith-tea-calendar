fabb337b5a425c12bf79acf0dea646987d657564
Nick Pleatsikas <nick@pleatsikas.me>
2025-12-29 21:59:30 -0800

    Read from/write to iCal file
    * Add argument "calendar" that specifies path to .ics file to store
      calendar events.
    * Added code to read and write/update calendar events to iCal file.
      Duplicate events are not written again and old events are not removed.
    * Exclude .ics files from vcs.

--- src/smith_tea_calendar/__main__.py
+++ src/smith_tea_calendar/__main__.py
@@ -1,6 +1,12 @@
 import asyncio
+import pathlib
+from datetime import date, datetime
 
 import click
+from ical.calendar import Calendar
+from ical.calendar_stream import IcsCalendarStream
+from ical.event import Event
+from ical.exceptions import CalendarParseError
 
 from .scraper import ScraperConfig, SmithTeaScraper
 
@@ -9,14 +15,52 @@ from .scraper import ScraperConfig, SmithTeaScraper
 @click.option("--email", required=True)
 @click.option("--password", required=True)
 @ScraperConfig.add_options
+@click.argument(
+    "calendar",
+    type=click.Path(
+        exists=False,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        writable=True,
+        path_type=pathlib.Path,
+    ),
+    default=pathlib.Path("orders.ics"),
+)
 @click.pass_context
-def cli(ctx: click.Context, **kwargs) -> None:
-    async def run():
-        scraper = SmithTeaScraper()
-        async for event in scraper.run(ctx):
-            print(event)
+def cli(ctx: click.Context, calendar: pathlib.Path, **kwargs) -> None:
+    asyncio.run(run(ctx, calendar))
 
-    asyncio.run(run())
+
+async def run(ctx: click.Context, calendar_file: pathlib.Path):
+    scraper = SmithTeaScraper()
+    calendar_file_exists = not calendar_file.exists()
+
+    with calendar_file.open("w+") as ics_file:
+        calendar = Calendar(prodid="github.com/mrflynn/smith-tea-calendar")
+
+        if calendar_file_exists:
+            try:
+                calendar = IcsCalendarStream.calendar_from_ics(ics_file.read())
+            except CalendarParseError:
+                pass
+
+        def event_keys(event: Event) -> tuple[date | datetime | str | None, ...]:
+            return (event.dtstart, event.summary, event.description)
+
+        calendar.events.extend(
+            filter(
+                lambda new_event: event_keys(new_event)
+                not in {
+                    event_keys(existing_event) for existing_event in calendar.events
+                },
+                [event async for event in scraper.run(ctx)],
+            )
+        )
+
+        ics_file.seek(0)
+        ics_file.write(IcsCalendarStream.calendar_to_ics(calendar))
+        ics_file.truncate()
 
 
 if __name__ == "__main__":
