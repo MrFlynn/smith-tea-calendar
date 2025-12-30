097526eff99e5e01bebd910fb499af10f553c888
Nick Pleatsikas <nick@pleatsikas.me>
2025-12-29 20:43:59 -0800

    Add basic CLI to project
    * Define project scripts and build system.
    * Add cli entrypoint. This just prints the raw events to stdout.

---
+++ src/smith_tea_calendar/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+
+import click
+
+from .scraper import ScraperConfig, SmithTeaScraper
+
+
+@click.command()
+@click.option("--username", required=True)
+@click.option("--password", required=True)
+@ScraperConfig.add_options
+@click.pass_context
+def cli(ctx: click.Context, **kwargs) -> None:
+    async def run():
+        scraper = SmithTeaScraper()
+        async for event in scraper.run(ctx):
+            print(event)
+
+    asyncio.run(run())
+
+
+if __name__ == "__main__":
+    cli()
